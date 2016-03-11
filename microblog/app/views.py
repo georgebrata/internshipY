c2cdcccd5d6330d1632af9d83e919df105b03720
George Brata <bratageorge@gmail.com>
2016-03-11 17:23:24 +0200

    Modularized HTML, CSS and JS

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -25,6 +25,7 @@ from flask import render_template, flash, redirect
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for OpenID="%s", remember_me=%s' %
